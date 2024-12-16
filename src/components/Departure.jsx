330365be3685d88407b2562a827fb5cbf497acaf
Fredrik Etsare <fredrik@etsare.se>
2024-12-16 11:28:36 +0100

    fixed responsiveness

--- src/components/Departure.jsx
+++ src/components/Departure.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./departure.css";
 import pendelIcon from "../assets/svg/pendel.svg";
 import bussIcon from "../assets/svg/buss.svg";
 import tunnelbanaIcon from "../assets/svg/tunnelbana.svg";
 import razor from "../assets/Razor.png";
+import "./departure.css"
 const iconMap = {
   Pendel: pendelIcon,
   Buss: bussIcon,
@@ -11,15 +11,18 @@ const iconMap = {
 };
 
 const Departure = ({ departures }) => {
-  if (!departures || departures.length === 0) {
-    return <div className="jahopp">Inga tåg avgår för tillfället</div>;
-  }
+  // if (!departures || departures.length === 0) {
+  //   return <div className="jahopp">Inga tåg avgår för tillfället</div>;
+  // }
+
+  const minRows = 5;
+  const placeholderRows = Math.max(minRows - departures.length, 0);
 
   return (
     <table>
       <thead>
         <tr className="cols">
-          <th className="departure-icon"><img src={razor} width={40} alt="" /></th>
+          <th className="departure-icon"></th>
           <th className="departure-name">Line</th>
           <th className="departure-time">Time</th>
           <th className="departure-direction">Direction</th>
@@ -34,16 +37,29 @@ const Departure = ({ departures }) => {
           const num = nameSplit[1];
 
           return (
-            <tr key={`departure-${index}`} className={index % 2 != 0 && "odd"}>
+            <tr key={`departure-${index}`} className={index % 2 !== 0 ? "odd" : ""}>
               <td className="departure-icon">
-                <img src={icon} alt={`${type} icon`} width={48} />
+                <img src={icon} alt={`${type} icon`}  />
               </td>
-              <td className="departure-name">
-                {num}
-              </td>
+              <td className="departure-name">{num}</td>
               <td className="departure-time">{departure.time}</td>
               <td className="departure-direction">{departure.direction.split(" ")[0]}</td>
-              <td className={`departure-time-left ${departure.timeLeft <= 10 && "red-text"}`}>{departure.timeLeft} min</td>
+              <td className={`departure-time-left ${departure.timeLeft <= 10 ? "red-text" : ""}`}>
+                {departure.timeLeft} min
+              </td>
+            </tr>
+          );
+        })}
+
+        {Array.from({ length: placeholderRows }).map((_, index) => {
+          const adjustedIndex = departures.length + index;
+          return (
+            <tr key={`placeholder-${index}`} className={adjustedIndex % 2 !== 0 ? "odd" : ""}>
+              <td className="departure-icon"><img src={razor} width={40} alt="" /></td>
+              <td className="departure-name"><img src={razor} width={40} alt="" /></td>
+              <td className="departure-time"><img src={razor} width={40} alt="" /></td>
+              <td className="departure-direction"><img src={razor} width={40} alt="" /></td>
+              <td className="departure-time-left"><img src={razor} width={40} alt="" /></td>
             </tr>
           );
         })}
